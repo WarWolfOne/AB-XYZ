764e8ac51246e74dbf89a257f3c6cb8646d193b5
WarWolfOne <matheussaito95k@gmail.com>
2021-11-29 14:43:47 -0300

    java

---
+++ Web/back/src/main/java/br/com/equilibrium/demo/EquilibriumApplication.java
@@ -0,0 +1,13 @@
+package br.com.equilibrium.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquilibriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquilibriumApplication.class, args);
+	}
+
+}
---
+++ Web/back/src/test/java/br/com/equilibrium/demo/EquilibriumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.equilibrium.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquilibriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
